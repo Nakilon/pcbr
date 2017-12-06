a988fd092182c523beaa663cee86e6f3fa3a3188
Victor Maslov <nakilon@gmail.com>
2017-12-06 23:01:02 +0300

    indentation

--- lib/pcbr.rb
+++ lib/pcbr.rb
@@ -8,49 +8,49 @@ module PCBR
 
   ARRAY_101 = [0, 0, 0]
   class Storage
-  attr_reader :table
+    attr_reader :table
 
-  def initialize &block
-    @table = []
-    @callback = block || lambda{ |a_, b_|
-      ARRAY_101.dup.tap do |array|
-        [*a_].zip([*b_]) do |a, b|
-          t = a <=> b and array[t] = t
-        end
-      end.inject :+
-    }
-  end
-
-  def store key, vector = nil
-    vector ||= Array key
-    score = 0
-    @table.each do |item|
-      # TODO test of this exception
-      fail "comparison vectors are of the different length" unless vector.size == item[1].size
-      point = @callback.call vector, item[1]
-      score += point
-      item[2] -= point
+    def initialize &block
+      @table = []
+      @callback = block || lambda{ |a_, b_|
+        ARRAY_101.dup.tap do |array|
+          [*a_].zip([*b_]) do |a, b|
+            t = a <=> b and array[t] = t
+          end
+        end.inject :+
+      }
     end
-    @table.push [key, vector, score]
+
+    def store key, vector = nil
+      vector ||= Array key
+      score = 0
+      @table.each do |item|
+        # TODO test of this exception
+        fail "comparison vectors are of the different length" unless vector.size == item[1].size
+        point = @callback.call vector, item[1]
+        score += point
+        item[2] -= point
+      end
+      @table.push [key, vector, score]
+    end
+
+    def score key
+      @table.assoc(key)[2]
+    end
+
+    def sorted
+      # from the best to the worst
+      @table.sort_by.with_index{ |item, i| [-item[2], i] }.map(&:first)
+    end
+
+    # def quality
+    #   factorial = ->x{ (1..x).inject(:*) }
+    #   (2...@table.size).each do |sublength|
+    #     combinations = factorial[@table.size] / factorial[sublength] / factorial[@table.size - sublength]
+    #     comparisons = sublength * (sublength - 1) / 2
+    #     p [sublength, combinations, comparisons, combinations * comparisons]
+    #   end
+    # end
+
   end
-
-  def score key
-    @table.assoc(key)[2]
-  end
-
-  def sorted
-    # from the best to the worst
-    @table.sort_by.with_index{ |item, i| [-item[2], i] }.map(&:first)
-  end
-
-  # def quality
-  #   factorial = ->x{ (1..x).inject(:*) }
-  #   (2...@table.size).each do |sublength|
-  #     combinations = factorial[@table.size] / factorial[sublength] / factorial[@table.size - sublength]
-  #     comparisons = sublength * (sublength - 1) / 2
-  #     p [sublength, combinations, comparisons, combinations * comparisons]
-  #   end
-  # end
-
-end
 end
